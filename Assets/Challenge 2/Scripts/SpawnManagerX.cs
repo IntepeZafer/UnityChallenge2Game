b2565f47a28b6823442e9c6ccc9b5e5f24318941
Zafer İntepe <108065413+IntepeZafer@users.noreply.github.com>
2025-03-04 03:56:13 +0300

    Finish

---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+    public float spawnLimitXLeft;
+    public float spawnLimitXRight;
+    public float spawnPosY;
+    public float startDelay;
+    public float spawnInterval;
+
+    private void Start()
+    {
+        Invoke("SpawnRandomBall", startDelay);
+    }
+
+    private void SpawnRandomBall()
+    {
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+        spawnInterval = Random.Range(3f, 5f);
+        Invoke("SpawnRandomBall" , spawnInterval);
+    }
+
+}
